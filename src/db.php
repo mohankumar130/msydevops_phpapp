115a237fb2cf7c526e1a606e9e445594c2f98fe6
mohankumar130 <mohankumar130@gmail.com>
2023-12-30 10:56:26 +0530

    updated

--- src/db.php
+++ src/db.php
@@ -1,17 +1,15 @@
 <?php
 //server with default setting (user 'root' with no password)
-$host = 'mysql-service';  // server 
-$user = 'root';   
-$pass = "12345678";   
-$database = 'demodb';   //Database Name  
+$host = 'mysql-service';  // server
+$user = 'root';
+$pass = "12345678";
+$database = 'demodb';   //Database Name
 
 // establishing connection
-  $conn = mysqli_connect($host,$user,$pass,$database);   
+  $conn = mysqli_connect($host,$user,$pass,$database);
 
  // for displaying an error msg in case the connection is not established
-  if (!$conn) {                                             
-    die("Connection failed: " . mysqli_connect_error());     
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
   }
 ?>
-
-
--- src/includes/delete.php
+++ src/includes/delete.php
@@ -9,7 +9,7 @@
          // SQL query to delete data from user table where id = $userid
          $query = "DELETE FROM users WHERE id = {$userid}"; 
          $delete_query= mysqli_query($conn, $query);
-         header("Location: home.php");
+         header("Location: index.php");
      }
               ?>
 
